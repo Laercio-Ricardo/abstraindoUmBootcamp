e6326e2f74032948856aad3225198fc2eabc8b27
Laercio <laercioricardolima@gmail.com>
2026-04-25 21:48:57 -0300

    Começando o projeto de Abstrair um Bootcamp
    CRIADO CURSOS

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import br.com.abstraindobootcamp.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
 
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java do Laércio");
+        curso1.setDescricao("Curso de Java feito por mim");
+        curso1.setCargaHoraria(8);
+
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso Java  Avançado");
+        curso2.setDescricao("Curso de Java Avançado");
+        curso2.setCargaHoraria(15);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
 
 
     }
